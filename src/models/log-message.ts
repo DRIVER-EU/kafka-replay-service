38a1ea2fbb98f1f521a2c35a06ab2665aeca7e2e
Erik Vullings <erik.vullings@gmail.com>
2018-03-03 16:12:33 +0100

    No longer needed

--- src/models/log-message.ts
+++
@@ -1,9 +0,0 @@
-// import { IJsonObject, IDefaultKey } from './message';
-
-// export interface ILogMessage {
-//   topic: string;
-//   partition: number;
-//   offset: number;
-//   key: IDefaultKey;
-//   value: IJsonObject;
-// }
